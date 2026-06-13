3e3ef34ad422b3c785ac29f540f6d8e101651d78
Hyun <simon.hareter@live.at>
2026-06-13 21:30:28 +0200

    nvim config as directory

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+require("vim._core.ui2").enable({})
+require("options")
+require("keymaps")
+require("commands")
+require("pack")
+require("treesitter")
+require("lsp")
+require("colorscheme")
---
+++ nvim/lua/colorscheme.lua
@@ -0,0 +1,5 @@
+vim.pack.add({
+   "https://github.com/metalelf0/black-metal-theme-neovim",
+})
+
+vim.cmd.colorscheme("dark-funeral")
